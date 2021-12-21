6ee1d1674bb1bee754e41e3bae5ffa34ddce8102
Vasyl Marusiak <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>
2021-12-21 06:57:27 +0200

    added sum method

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,4 +2,9 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Hello worldff!" + "!");
     }
+
+    public static int sum(int a, int b) {
+        int c = a + b;
+        return c;
+    }
 }
